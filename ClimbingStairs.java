ac310612140f0c3c56d65ac24fd6f89d70162569
ANUBHAV MAURYA <67480490+AnubhavMaurya@users.noreply.github.com>
2021-10-03 01:05:25 +0530

    Update ClimbingStairs.java

--- ClimbingStairs.java
+++ ClimbingStairs.java
@@ -1,7 +1,7 @@
 class Solution {
     public int climbStairs(int n) {
         if(n<=2)
-            return n;
+           return n;
         int a=1,b=2,sum=0;
         for(int i=3;i<=n;i++)
         {
