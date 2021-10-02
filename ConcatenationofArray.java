e1ffaeb5380e2f81cf642f51cc542510fb899ff0
ANUBHAV MAURYA <67480490+AnubhavMaurya@users.noreply.github.com>
2021-10-02 23:44:07 +0530

    Create ConcatenationofArray.java
    Concatenation of Array
    
    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
    
    Specifically, ans is the concatenation of two nums arrays.
    
    Return the array ans.
    
     
    
    Example 1:
    
    Input: nums = [1,2,1]
    Output: [1,2,1,1,2,1]
    Explanation: The array ans is formed as follows:
    - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
    - ans = [1,2,1,1,2,1]
    Example 2:
    
    Input: nums = [1,3,2,1]
    Output: [1,3,2,1,1,3,2,1]
    Explanation: The array ans is formed as follows:
    - ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
    - ans = [1,3,2,1,1,3,2,1]

---
+++ ConcatenationofArray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int a[]=new int[2*nums.length];
+        int j=0;
+        for(int i=0;i<2*nums.length;i++)
+        {
+            a[i]=nums[j];
+            j++;
+            if(j>=nums.length)
+                j=0;
+        }
+        return a;
+    }
+}
