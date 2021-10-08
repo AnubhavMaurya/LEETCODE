0387d570568d18fbf7f3766eb1d88008bbbc52ba
ANUBHAV MAURYA <67480490+AnubhavMaurya@users.noreply.github.com>
2021-10-08 23:01:20 +0530

    Update HammingDistance.java

--- HammingDistance.java
+++ HammingDistance.java
@@ -2,7 +2,7 @@ class Solution {
     public int hammingDistance(int x, int y) {
        int result=x^y;
         String str=Integer.toBinaryString(result);
-       int sum=0;
+        int sum=0;
             for(int i=str.length()-1;i>=0;i--)
             {
            if(str.charAt(i)=='1')
