0162c4a363d0ed4bb2b97c95f9b491f36c6a6c9e
ANUBHAV MAURYA <67480490+AnubhavMaurya@users.noreply.github.com>
2021-10-20 23:52:30 +0530

    Create CheckIfTheSentenceIsPangram.java
     Check if the Sentence Is Pangram
    
    A pangram is a sentence where every letter of the English alphabet appears at least once.
    
    Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
    
     
    
    Example 1:
    
    Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
    Output: true
    Explanation: sentence contains at least one of every letter of the English alphabet.
    Example 2:
    
    Input: sentence = "leetcode"
    Output: false

---
+++ CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+    int check=0;
+        for(char ch='a';ch<='z';ch++)
+    {
+        check=sentence.indexOf(ch);
+            System.out.println(check);
+            if(check==-1)
+                return false;
+    }
+        return true;
+    }
+}
